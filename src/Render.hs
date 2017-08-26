59678f3d7f22064338d75a82a69f036560c71854
adamrk <ark.email@gmail.com>
2017-08-25 23:19:55 -0400

    show previous answer in prompt

--- src/Render.hs
+++ src/Render.hs
@@ -3,7 +3,11 @@ module Render where
 import GameState
 
 printGame :: GState -> String
-printGame (GState (Characters cs) (Current n) _) =
-  cs ++ "\n" ++ indicator ++ "\n"
+printGame (GState (Characters cs) (Current n) a _) =
+  cs ++ "\n" ++ indicator ++ "\n" ++ previous ++ lastAnswer ++ "\n"
   where
     indicator = replicate n ' ' ++ "^"
+    previous = take n cs
+    lastAnswer = case a of
+      Just (Answer a) -> [a]
+      Nothing -> ""
